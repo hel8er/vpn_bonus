e3e65f1d089aa805417aad0ad2e5e561777ba2ad
hel8er <helber-alx@mail.ru>
2022-05-02 11:32:06 +0300

    async webhook

--- bot/webhook.py
+++ bot/webhook.py
@@ -1,12 +1,12 @@
+import crud
+import aiohttp
 import urllib.parse
-from .schemas import Update
-import requests
-
+from schemas import Update
 
 class Command:
     text = ''
 
-    def run(self, update: Update):
+    async def run(self, update: Update):
         """method to ovverride"""
         pass
 
@@ -18,7 +18,7 @@ class Webhook:
         self.base_url = f"https://api.telegram.org/bot{token}/"
         self.commands = {cmd.text: cmd for cmd in Command.__subclasses__()}
 
-    def request(self, method, **params):
+    async def request(self, method, **params):
         url = self.base_url + method
         if params:
 
@@ -26,28 +26,36 @@ class Webhook:
 
             url += params_str
 
-            response = requests.request("GET", url,)
-            return response.json()
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            async with session.get(url) as response:
+                return await response.json()
 
-    def get_me(self):
-        info = self.request('getMe')
+    async def get_me(self):
+        info = await self.request('getMe')
         return info
 
-    def forward_message(self, from_id: int, to_id: int, msg_id: int):
-        res = self.request('forwardMessage', chat_id=to_id, from_chat_id=from_id, message_id=msg_id)
+    async def forward_message(self, from_id: int, to_id: int, msg_id: int):
+        res = await self.request('forwardMessage', chat_id=to_id, from_chat_id=from_id, message_id=msg_id)
         return res
 
-    def send_message(self, chat_id: int, message: str):
-        res = self.request('sendMessage', chat_id=chat_id, text=message)
+    async def send_message(self, chat_id: int, message: str):
+        res = await self.request('sendMessage', chat_id=chat_id, text=message)
         return res
 
-    def set_webhook(self, url):
-        return self.request('setWebhook', url=url)
+    async def set_webhook(self, url):
+        return await self.request('setWebhook', url=url)
 
-    def delete_webhook(self):
-        return self.request('deleteWebhook')
+    async def delete_webhook(self):
+        return await self.request('deleteWebhook')
 
-    def webhook_info(self):
-        info = self.request('getWebhookInfo')
+    async def webhook_info(self):
+        info = await self.request('getWebhookInfo')
         print(info)
         return info
+
+    def command(self, fn):
+        def wrapper(update: Update, text: str):
+            if text == update.message.text:
+                return fn(update, text)
+
+        return wrapper
